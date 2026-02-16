5bdce3c2ca61426895326440337563c23526beb8
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-16 15:54:17 -0500

    Actualizacion de Logo

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ import Logo from '@/components/Logo'
 export default function AppLayout() {
   return (
     <>
-      <header className='bg-gray-800 py-5'>
+      <header className='bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 border-b border-white/5 shadow-sm py-5'>
         <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
           <div className='w-64'>
             <Logo />
