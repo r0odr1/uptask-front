5aeb10829b3e26a2dccc76b55f2b806816800ad4
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-16 17:02:41 -0500

    Creando la pagina para crear proyectos

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom"
 
-export default function DashboardView() {
+export default function CreateProjectView() {
   return (
     <>
-      <h1 className="text-5xl font-black">Mis Proyectos</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Maneja y administra tus proyectos</p>
+      <h1 className="text-5xl font-black">Crear Proyecto</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario para crear un proyecto</p>
 
       <nav className="my-5">
         <Link
           className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
-          to='/projects/create'
+          to='/'
         >
-          Nuevo Proyecto
+          Volver a Proyectos
         </Link>
       </nav>
     </>
