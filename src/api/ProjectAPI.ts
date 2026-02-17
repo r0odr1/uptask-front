69bea537967b54e9ba404a04f15f95f07d8ed80c
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-17 16:42:54 -0500

    Leer errores del back con useMutation

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -1,5 +1,6 @@
 import api from "@/lib/axios";
 import { ProjectFormData } from "@/types/index";
+import { isAxiosError } from "axios";
 
 
 export async function createProject(formData : ProjectFormData) {
@@ -9,6 +10,8 @@ export async function createProject(formData : ProjectFormData) {
     return data
 
   } catch (error) {
-    console.log(error)
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
   }
 }
--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -20,8 +20,8 @@ export default function CreateProjectView() {
   const { mutate } = useMutation({
     mutationFn:
       createProject,
-      onError: () => {
-
+      onError: (error) => {
+        toast.error(error.message)
       },
       onSuccess: (data) => {
         toast.success(data)
