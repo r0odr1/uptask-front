17b4d1e87071f4749a63393393f2bfa2b97e2111
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-17 13:26:01 -0500

    Creando el formulario para la creacion de proyectos

--- src/views/projects/CreateProjectView.tsx
+++ src/views/projects/CreateProjectView.tsx
@@ -1,19 +1,55 @@
+import ProjectForm from "@/components/projects/ProjectForm"
+import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
 
 export default function CreateProjectView() {
+
+  const initialValues = {
+    projectName: "",
+    clientName: "",
+    description: ""
+}
+
+  const {register, handleSubmit, formState: {errors}} = useForm({defaultValues: initialValues})
+
+  const handleForm = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
-      <h1 className="text-5xl font-black">Crear Proyecto</h1>
-      <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario para crear un proyecto</p>
+      <div className="max-w-3xl mx-auto">
+        <h1 className="text-5xl font-black">Crear Proyecto</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario para crear un proyecto</p>
 
-      <nav className="my-5">
-        <Link
-          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
-          to='/'
+        <nav className="my-5">
+          <Link
+            className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
+            to='/'
+          >
+            Volver a Proyectos
+          </Link>
+        </nav>
+
+        <form
+          action=""
+          className="mt-10 bg-white shadow-lg p-10 rounded-lg"
+          onSubmit={handleSubmit(handleForm)}
+          noValidate
         >
-          Volver a Proyectos
-        </Link>
-      </nav>
+
+          <ProjectForm
+            register={register}
+            errors={errors}
+          />
+
+          <input
+            type="submit"
+            value='Crear Proyecto'
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors rounded-lg"
+          />
+        </form>
+      </div>
     </>
   )
 }
