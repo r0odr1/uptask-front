777bd350f890509ad922a5aea4d146366b01c656
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-18 18:48:17 -0500

    Mostrando la informacion de las tareas en el modal

--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -4,6 +4,7 @@ import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 import { getTaskById } from '@/api/TaskAPI';
 import { toast } from 'react-toastify';
+import { formatDate } from '@/utils/utils';
 
 
 export default function TaskModalDetails() {
@@ -58,8 +59,8 @@ export default function TaskModalDetails() {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
-                  <p className='text-sm text-slate-400'>Agregada el: </p>
-                  <p className='text-sm text-slate-400'>Última actualización: </p>
+                  <p className='text-sm text-slate-400'>Agregada el: {formatDate(data.createdAt)}</p>
+                  <p className='text-sm text-slate-400'>Última actualización: {formatDate(data.updatedAt)}</p>
 
                   <Dialog.Title
                     as="h3"
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,6 +8,8 @@ export const taskSchema = z.object({
   description: z.string(),
   project: z.string(),
   status: taskStatusSchema,
+  createdAt: z.string(),
+  updatedAt: z.string()
 })
 
 export type Task = z.infer<typeof taskSchema>
