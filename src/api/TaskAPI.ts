77c0fe6648cb030a71fb9f988c6f97fd3917b9c9
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-18 18:35:13 -0500

    Validando con ZOD la respuesta de la API

--- src/api/TaskAPI.ts
+++ src/api/TaskAPI.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "@/lib/axios";
-import { Project, Task, TaskFormData } from "../types";
+import { Project, Task, TaskFormData, taskSchema } from "../types";
 
 type TaskAPI = {
   formData: TaskFormData
@@ -27,7 +27,11 @@ export async function getTaskById({projectId, taskId} : Pick<TaskAPI, 'projectId
   try {
     const url = `/projects/${projectId}/tasks/${taskId}`
     const { data } = await api(url)
-    return data
+    const response = taskSchema.safeParse(data)
+
+    if(response.success) {
+      return response.data
+    }
 
   } catch (error) {
     if(isAxiosError(error) && error.response) {
--- src/components/tasks/TaskModalDetails.tsx
+++ src/components/tasks/TaskModalDetails.tsx
@@ -30,7 +30,7 @@ export default function TaskModalDetails() {
     return <Navigate to={`/projects/${projectId}`} />
   }
 
-  return (
+  if(data) return (
     <>
       <Transition appear show={show} as={Fragment}>
         <Dialog as="div" className="relative z-10" onClose={() => navigate(location.pathname, {replace: true})}>
@@ -60,15 +60,17 @@ export default function TaskModalDetails() {
                 <Dialog.Panel className="w-full max-w-4xl transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all p-16">
                   <p className='text-sm text-slate-400'>Agregada el: </p>
                   <p className='text-sm text-slate-400'>Última actualización: </p>
+
                   <Dialog.Title
                     as="h3"
                     className="font-black text-4xl text-slate-600 my-5"
                   >
-                    Titulo aquí
+                    {data.name}
                   </Dialog.Title>
-                  <p className='text-lg text-slate-500 mb-2'>Descripción:</p>
+
+                  <p className='text-lg text-slate-500 mb-2'>Descripción: {data.description}</p>
                   <div className='my-5 space-y-3'>
-                    <label className='font-bold'>Estado Actual:</label>
+                    <label className='font-bold'>Estado Actual: {data.status}</label>
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
