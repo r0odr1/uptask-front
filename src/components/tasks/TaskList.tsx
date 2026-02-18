9de147c4c17e4c119bea316f246dcac84efd75df
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-18 15:14:33 -0500

    Mostrando el estado de la tarea a la que pertenece cada una

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -17,6 +17,14 @@ const initialStatusGroups : GroupedTasks = {
   completed: []
 }
 
+const statusTranslations : {[key: string] : string} = {
+  pending:'Pendiente' ,
+  onHold: 'En Espera',
+  inProgress: 'En Progreso',
+  underReview: 'En Revisión',
+  completed: 'Completado'
+}
+
 export default function TaskList({tasks} : TaskListProps) {
 
   const groupedTasks = tasks.reduce((acc, task) => {
@@ -35,6 +43,11 @@ export default function TaskList({tasks} : TaskListProps) {
       <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
         {Object.entries(groupedTasks).map(([status, tasks]) => (
           <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+            <h3
+              className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 rounded-lg`}
+            >
+              {statusTranslations[status]}
+            </h3>
             <ul className='mt-5 space-y-5'>
               {tasks.length === 0 ? (
                 <li className="text-gray-500 text-center pt-3">No Hay tareas</li>
