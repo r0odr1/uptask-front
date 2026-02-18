0498b070c0467e4b73295fff2d2346aedca005f5
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-18 12:15:55 -0500

    Creando la ruta y routing hacia el detalle del proyecto

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Logo from '@/components/Logo'
@@ -10,7 +10,9 @@ export default function AppLayout() {
       <header className='bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 border-b border-white/5 shadow-sm py-5'>
         <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
           <div className='w-64'>
-            <Logo />
+            <Link to={'/'}>
+              <Logo />
+            </Link>
           </div>
 
           <NavMenu />
