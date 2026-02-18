c479408b16f5c8111184cbbbdda19e6bc18f63de
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-18 11:17:38 -0500

    Eliminar proyectos con useMutation

--- src/api/ProjectAPI.ts
+++ src/api/ProjectAPI.ts
@@ -64,3 +64,16 @@ export async function updateProject({formData, projectId} : ProjectAPIType) {
     }
   }
 }
+
+export async function deleteProject(id: Project['_id']) {
+
+  try {
+    const { data } = await api.delete<string>(`/projects/${id}`)
+    return data
+
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
