33d2fb6bcd4a200e0bdad2156f0be692d32efdf1
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-20 16:57:39 -0500

    Enviando el nuevo codigo de confirmacion

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from '@/lib/axios'
 import { isAxiosError } from 'axios'
-import { ConfirmToken, UserRegistrationForm } from '../types'
+import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from '../types'
 
 export async function createAccount(formData: UserRegistrationForm) {
 
@@ -20,6 +20,20 @@ export async function confirmAccount(formData: ConfirmToken) {
 
   try {
     const url = '/auth/confirm-account'
+    const { data } = await api.post<string>(url, formData)
+
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function requestConfirmationCode(formData: RequestConfirmationCodeForm) {
+
+  try {
+    const url = '/auth/request-code'
     const { data } = await api.post<string>(url, formData)
 
     return data
--- src/router.tsx
+++ src/router.tsx
@@ -8,6 +8,7 @@ import AuthLayout from './layouts/AuthLayout'
 import LoginView from './views/auth/LoginView'
 import RegisterView from './views/auth/RegisterView'
 import ConfirmAccountView from './views/auth/ConfirmAccountView'
+import RequestNewCodeView from './views/auth/RequestNewCodeView'
 
 export default function Router() {
 
@@ -22,9 +23,10 @@ export default function Router() {
         </Route>
 
         <Route element={<AuthLayout />} >
-          <Route path='/auth/login' element={<LoginView />} index />
-          <Route path='/auth/register' element={<RegisterView />} index />
-          <Route path='/auth/confirm-account' element={<ConfirmAccountView />} index />
+          <Route path='/auth/login' element={<LoginView />} />
+          <Route path='/auth/register' element={<RegisterView />} />
+          <Route path='/auth/confirm-account' element={<ConfirmAccountView />} />
+          <Route path='/auth/request-code' element={<RequestNewCodeView />} />
         </Route>
 
       </Routes>
