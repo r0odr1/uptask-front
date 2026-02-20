c4fb369df3ccbdd9c6a2dac90520fd4aad02e49e
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-20 14:09:48 -0500

    Creando la vista para el registro y routing

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,6 +10,7 @@ export const authSchema = z.object({
 
 type Auth = z.infer<typeof authSchema>
 export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+export type UserRegistrationForm = Pick<Auth, 'name' | 'email' | 'password' | 'password_confirmation'>
 
 /** Task */
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import { UserLoginForm } from "@/types/index";
 import ErrorMessage from "@/components/ErrorMessage";
+import { Link } from "react-router-dom";
 
 export default function LoginView() {
 
@@ -66,6 +67,15 @@ export default function LoginView() {
           className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white font-black text-xl cursor-pointer rounded-lg"
         />
       </form>
+
+      <nav className="mt-10 flex flex-col space-y-4">
+        <Link
+          to={'/auth/register'}
+          className="text-center text-gray-300 font-normal"
+        >
+          ¿No tienes una cuenta? Crear Una
+        </Link>
+      </nav>
     </>
   )
 }
