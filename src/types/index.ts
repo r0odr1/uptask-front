ac6b006b390bf459909cc19ff4033792bb5f8613
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-20 13:56:54 -0500

    Creando la vista de login y schema de Zod para el formulario

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,16 @@
 import { z } from 'zod'
+
+/** Auth & Users */
+export const authSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  password_confirmation: z.string()
+})
+
+type Auth = z.infer<typeof authSchema>
+export type UserLoginForm = Pick<Auth, 'email' | 'password'>
+
 /** Task */
 export const taskStatusSchema = z.enum(["pending", "onHold", "inProgress", "underReview", "completed"])
 export type TaskStatus = z.infer<typeof taskStatusSchema>
