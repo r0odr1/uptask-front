0526eaa1b5efafcfdd31c34262efeedca5abdf7c
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-20 18:26:20 -0500

    creando la funcionalidad para autenticar y actualizando logo

--- src/api/AuthAPI.ts
+++ src/api/AuthAPI.ts
@@ -1,6 +1,6 @@
 import api from '@/lib/axios'
 import { isAxiosError } from 'axios'
-import { ConfirmToken, RequestConfirmationCodeForm, UserRegistrationForm } from '../types'
+import { ConfirmToken, RequestConfirmationCodeForm, UserLoginForm, UserRegistrationForm } from '../types'
 
 export async function createAccount(formData: UserRegistrationForm) {
 
@@ -34,6 +34,20 @@ export async function requestConfirmationCode(formData: RequestConfirmationCodeF
 
   try {
     const url = '/auth/request-code'
+    const { data } = await api.post<string>(url, formData)
+
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function authenticateUser(formData: UserLoginForm) {
+
+  try {
+    const url = '/auth/login'
     const { data } = await api.post<string>(url, formData)
 
     return data
