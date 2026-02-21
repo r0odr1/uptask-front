b16c8bf772fdf09e7e340951c10bc3b6f9113d21
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-20 19:48:57 -0500

    Nueva ruta para reestablecer contrasena

--- src/views/auth/NewPasswordView.tsx
+++ src/views/auth/NewPasswordView.tsx
@@ -1,6 +1,20 @@
+import NewPasswordToken from "@/components/auth/NewPasswordToken"
+import NewPasswordForm from "@/components/auth/NewPasswordForm"
+import { useState } from "react"
 
 export default function NewPasswordView() {
+
+  const [isValidToken, setIsValidToken] = useState(false)
+
   return (
-    <div>NewPasswordView</div>
+    <>
+      <h1 className="text-5xl font-black text-white">Reestablecer Contraseña</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Ingresa el código que recibiste {''}
+        <span className=" text-fuchsia-500 font-bold"> por correo</span>
+      </p>
+
+      {!isValidToken ? <NewPasswordToken /> : <NewPasswordForm /> }
+    </>
   )
 }
