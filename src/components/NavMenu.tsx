4483e6755775ead6d565a949471cb3b6ea7a2ed9
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-23 18:14:30 -0500

    agregar cerrar sesion

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -3,13 +3,19 @@ import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom'
 import { User } from '../types'
+import { useQueryClient } from '@tanstack/react-query'
 
 type NavMenuProps = {
   name: User['name']
-
 }
 
 export default function NavMenu({name} : NavMenuProps) {
+
+  const queryClient = useQueryClient()
+  const logout = () => {
+    localStorage.removeItem('AUTH_TOKEN')
+    queryClient.invalidateQueries({queryKey: ['user']})
+  }
 
   return (
     <Popover className="relative">
@@ -40,7 +46,7 @@ export default function NavMenu({name} : NavMenuProps) {
             <button
               className='block p-2 hover:text-purple-950'
               type='button'
-              onClick={() => { }}
+              onClick={logout}
             >
               Cerrar Sesión
             </button>
