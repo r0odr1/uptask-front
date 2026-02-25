c888c9f3fb0c65dd4e8f354ff34f0d16e0e7da03
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-25 13:50:44 -0500

    creando rutas para colaboradores

--- src/views/projects/ProjectDetailsView.tsx
+++ src/views/projects/ProjectDetailsView.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { Link, Navigate, useNavigate, useParams } from "react-router-dom"
 import { useQuery } from '@tanstack/react-query'
 import { getProjectById } from "@/api/ProjectAPI"
 import AddTaskModal from "@/components/tasks/AddTaskModal"
@@ -28,7 +28,7 @@ export default function ProjectDetailsView() {
       <h1 className="text-5xl font-black">{data.projectName}</h1>
       <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
 
-      <nav className="my-5">
+      <nav className="my-5 flex gap-3">
         <button
           type="button"
           className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
@@ -36,6 +36,13 @@ export default function ProjectDetailsView() {
         >
           Agregar Tarea
         </button>
+
+        <Link
+          to={'team'}
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
+        >
+          Colaboradores
+        </Link>
       </nav>
 
       <TaskList
---
+++ src/views/projects/ProjectTeamView.tsx
@@ -0,0 +1,33 @@
+import { Link, useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+
+export default function ProjectTeamView() {
+
+  const navigate = useNavigate()
+  const params = useParams()
+  const projectId = params.projectId!
+
+  return (
+    <>
+      <h1 className="text-5xl font-black">Administrar Equipo</h1>
+      <p className="text-2xl font-light text-gray-500 mt-5">Administra el equipo de trabajo para este proyecto</p>
+
+      <nav className="my-5 flex gap-3">
+        <button
+          type="button"
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
+          onClick={() => navigate(location.pathname + '?addMember=true')}
+        >
+          Agregar Colaborador
+        </button>
+
+        <Link
+          to={`/projects/${projectId}`}
+          className="bg-fuchsia-600 hover:bg-fuchsia-700 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors rounded-lg"
+        >
+          Volver a Proyecto
+        </Link>
+      </nav>
+    </>
+  )
+}
