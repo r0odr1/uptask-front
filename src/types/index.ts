77da76932e2f65c83ba8ea6eb1760fdce26bdac4
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-02 15:08:07 -0500

    Restringiendo las acciones para el proyecto

--- src/types/index.ts
+++ src/types/index.ts
@@ -51,6 +51,7 @@ export const projectSchema = z.object({
   projectName: z.string(),
   clientName: z.string(),
   description: z.string(),
+  manager: z.string()
 })
 
 export const dashboardProjectSchema = z.array(
@@ -58,7 +59,8 @@ export const dashboardProjectSchema = z.array(
     _id: true,
     projectName: true,
     clientName: true,
-    description: true
+    description: true,
+    manager: true
   })
 )
 
