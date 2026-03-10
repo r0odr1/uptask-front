b450a57e42499df33596a65abf08b87eee0d7c91
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-10 13:17:07 -0500

    Agregando funcion donde solo el usuario que creo la nota puede eliminar

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,11 +1,19 @@
+import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMemo } from "react"
 
 type NoteDetailProps = {
   note: Note
 }
 
 export default function NoteDetail({note} : NoteDetailProps) {
+
+  const { data, isLoading} = useAuth()
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+  if(isLoading) return 'Cargando...'
+
   return (
     <div className="p-3 flex justify-between items-center">
       <div>
@@ -16,6 +24,13 @@ export default function NoteDetail({note} : NoteDetailProps) {
           {formatDate(note.createdAt)}
         </p>
       </div>
+
+      {canDelete && (
+        <button
+          type="button"
+          className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors rounded-lg"
+        >Eliminar</button>
+      )}
     </div>
   )
 }
