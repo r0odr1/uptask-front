066e59d4fb3abcad85e1cdfc6bf095b24b9fc925
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-11 11:16:28 -0500

    Eliminando notas e invalidando quries

--- src/components/notes/NoteDetail.tsx
+++ src/components/notes/NoteDetail.tsx
@@ -1,7 +1,11 @@
+import { deleteNote } from "@/api/NoteAPI"
 import { useAuth } from "@/hooks/useAuth"
 import { Note } from "@/types/index"
 import { formatDate } from "@/utils/utils"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useMemo } from "react"
+import { useLocation, useParams } from "react-router-dom"
+import { toast } from "react-toastify"
 
 type NoteDetailProps = {
   note: Note
@@ -10,7 +14,24 @@ type NoteDetailProps = {
 export default function NoteDetail({note} : NoteDetailProps) {
 
   const { data, isLoading} = useAuth()
-  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data, note.createdBy._id])
+  const params = useParams()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+
+  const taskId = queryParams.get('viewTask')!
+  const projectId = params.projectId!
+
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onError: (error) => toast.error(error.message),
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({queryKey: ['task', taskId]})
+    }
+  })
 
   if(isLoading) return 'Cargando...'
 
@@ -29,6 +50,7 @@ export default function NoteDetail({note} : NoteDetailProps) {
         <button
           type="button"
           className="bg-red-400 hover:bg-red-500 p-2 text-xs text-white font-bold cursor-pointer transition-colors rounded-lg"
+          onClick={() => mutate({projectId, taskId, noteId: note._id})}
         >Eliminar</button>
       )}
     </div>
