66ab13cdf7e9b553f38aac9b2f1317f34e67dfa7
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-11 20:23:28 -0500

    Routing para las rutas de perfil

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,12 +1,10 @@
+import Tabs from "@/components/profile/Tabs";
 import { Outlet } from "react-router-dom";
 
 export default function ProfileLayout() {
   return (
     <>
-      <div>
-
-      </div>
-
+      <Tabs />
       <Outlet />
     </>
   )
