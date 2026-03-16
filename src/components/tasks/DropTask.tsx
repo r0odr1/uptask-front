e26943c750413ec8bac7e7bd4f9129c39342e849
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-16 15:09:20 -0500

    Agregar efecto al Drag

--- src/components/tasks/DropTask.tsx
+++ src/components/tasks/DropTask.tsx
@@ -6,12 +6,17 @@ type DropTaskProps = {
 export default function DropTask({status} : DropTaskProps) {
 
   const { isOver, setNodeRef } = useDroppable({
-      id: status
-    })
+    id: status
+  })
+
+  const style = {
+    opacity: isOver ? 0.4 : undefined
+  }
 
   return (
     <div
-    ref={setNodeRef}
+      style={style}
+      ref={setNodeRef}
       className="text-xs font-semibold uppercase p-2 border border-dashed border-slate-500 mt-5 grid place-content-center text-slate-500 rounded-lg"
     >
       Soltar tarea aqui
